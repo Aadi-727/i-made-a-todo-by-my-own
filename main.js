114ffc37e8c9eaca625713e207ae86c8c3ee0f2b
= <adithyanas1@gmail.com>
2021-09-10 13:32:28 +0530

    lclStr-01 bugfx-1

--- main.js
+++ main.js
@@ -10,22 +10,22 @@ class Todo {
 
 class Display {
 	static displayTodos() {
-		let StoredTodos = [
-			{
-				title: "paint",
-				isCompleted: true,
-			},
-			{
-				title: "code",
-				isCompleted: true,
-			},
-			{
-				title: "Clean the room",
-				isCompleted: false,
-			},
-		];
+		// let StoredTodos = [
+		// 	{
+		// 		title: "paint",
+		// 		isCompleted: true,
+		// 	},
+		// 	{
+		// 		title: "code",
+		// 		isCompleted: true,
+		// 	},
+		// 	{
+		// 		title: "Clean the room",
+		// 		isCompleted: false,
+		// 	},
+		// ];
 
-		const todos = StoredTodos;
+		const todos = Storage.getTodos();
 		todos.forEach((todo) => Display.addTodoToUI(todo));
 		// todos
 		// 	.filter((todo) => todo.isCompleted === true)
@@ -72,6 +72,40 @@ class Display {
 			alertBox.remove();
 		}, 3000);
 	}
+	static clearFlied() {
+		document.getElementById("input").value = "";
+	}
+}
+////////////////////////////////////////////////////////////////////
+// ? Storage
+
+class Storage {
+	static getTodos() {
+		let todos = [];
+		if (localStorage.getItem("todos") === null) {
+			todos = [];
+		} else {
+			todos = JSON.parse(localStorage.getItem("todos"));
+		}
+		return todos;
+	}
+	static addTodo(todo) {
+		const todos = Storage.getTodos();
+		todos.push(todo);
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}
+	static removeTodo(title) {
+		let todos = Storage.getTodos();
+		todos = todos.filter((todo) => {
+			return todo.title != title;
+		});
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}
+	static removeCompleted(complete) {
+		let todos = Storage.getTodos();
+		todos = complete.forEach((todo) => todo.filter((t) => t.checked != true));
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}
 }
 
 // ! Dead
@@ -87,21 +121,33 @@ document.addEventListener("DOMContentLoaded", Display.displayTodos);
 //* With add Btn
 document.getElementById("add").addEventListener("click", (e) => {
 	const title = document.getElementById("input").value;
+	if (title === "") {
+		Display.displayAlert("Please type in a Todo", "danger");
+	} else {
+		const todo = new Todo(title, false);
+		Display.addTodoToUI(todo);
+		// ? storage
+		Storage.addTodo(todo);
 
-	const todo = new Todo(title, false);
-	Display.addTodoToUI(todo);
-	Display.displayAlert("Todo Added", "success");
-	document.getElementById("input").value = "";
+		Display.displayAlert("Todo Added", "success");
+		Display.clearFlied();
+	}
 });
 
 // * Witheeee Enter key
 document.getElementById("input").addEventListener("keypress", (e) => {
 	if (e.key === "Enter") {
 		const title = document.getElementById("input").value;
-		const todo = new Todo(title, false);
-		Display.addTodoToUI(todo);
-		Display.displayAlert("Todo Added", "success");
-		document.getElementById("input").value = "";
+		if (title === "") {
+			Display.displayAlert("Please type in a Todo", "danger");
+		} else {
+			const todo = new Todo(title, false);
+			Display.addTodoToUI(todo);
+			Storage.addTodo(todo);
+			Display.displayAlert("Todo Added", "success");
+			document.getElementById("input").value = "";
+			Display.clearFlied();
+		}
 	}
 });
 
@@ -110,6 +156,7 @@ document.getElementById("input").addEventListener("keypress", (e) => {
 const todoUl = document.getElementById("todo");
 todoUl.addEventListener("click", (e) => {
 	Display.deleteTodo(e.target);
+	Storage.removeTodo(e.target.parentElement.previousElementSibling.textContent);
 	Display.displayAlert("Todo Deleted", "danger");
 });
 
@@ -117,6 +164,9 @@ todoUl.addEventListener("click", (e) => {
 document.getElementById("completed").addEventListener("click", (e) => {
 	const checkboxChecked = document.querySelectorAll("#check-box");
 	Display.deleteCompleted(
+		[...checkboxChecked].filter((check) => check.checked === true)
+	);
+	Storage.removeCompleted(
 		[...checkboxChecked].filter((check) => check.checked === true)
 	);
 	Display.displayAlert("Completed Todos Deleted", "danger");
